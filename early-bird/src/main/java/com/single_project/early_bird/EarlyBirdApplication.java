a5bb3eb14e34f29a32a15b9d880fabdec2dfb5a2
j00r6 <jindory8@gmail.com>
2024-10-17 14:43:14 +0900

    chore : 디렉토리 구조 설정

---
+++ early-bird/src/main/java/com/single_project/early_bird/EarlyBirdApplication.java
@@ -0,0 +1,13 @@
+package com.single_project.early_bird;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EarlyBirdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EarlyBirdApplication.class, args);
+	}
+
+}
---
+++ early-bird/src/test/java/com/single_project/early_bird/EarlyBirdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.single_project.early_bird;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+class EarlyBirdApplicationTests {
+
+//	@Test
+	void contextLoads() {
+	}
+
+}
