a799c94b27cefda0fc2c0fb60970b8e40d5b233e
j00r6 <jindory8@gmail.com>
2024-10-22 00:37:28 +0900

    feat : Exception 추가 (JWT 인증 관련)

---
+++ early-bird/src/main/java/com/single_project/early_bird/Global/exception/IllegalToken.java
@@ -0,0 +1,7 @@
+package com.single_project.early_bird.Global.exception;
+
+public class IllegalToken extends RuntimeException {
+    public IllegalToken(String message) {
+        super(message);
+    }
+}
