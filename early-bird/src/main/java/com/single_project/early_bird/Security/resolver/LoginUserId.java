f7bc16fb98c0cc1ca9ac01b8c341565e3ec39006
j00r6 <jindory8@gmail.com>
2024-10-22 23:20:19 +0900

    fix : Resolver Fix

--- early-bird/src/main/java/com/single_project/early_bird/Security/resolver/LoginUserId.java
+++ early-bird/src/main/java/com/single_project/early_bird/Security/resolver/LoginUserId.java
@@ -7,5 +7,5 @@ import java.lang.annotation.Target;
 
 @Target(ElementType.PARAMETER) // 메서드의 파라미터에 적용
 @Retention(RetentionPolicy.RUNTIME) // 어노테이션의 수명주기를 런타임 동안
-public @interface LoginMemberId {
+public @interface LoginUserId {
 }
