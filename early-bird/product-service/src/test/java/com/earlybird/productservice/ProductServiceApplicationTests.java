62836ae8d78a0b70e4117299d286b79ed9b2c332
j00r6 <jindory8@gmail.com>
2024-10-29 14:42:38 +0900

    fix : product-service 모듈 생성

---
+++ early-bird/product-service/src/test/java/com/earlybird/productservice/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.earlybird.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
