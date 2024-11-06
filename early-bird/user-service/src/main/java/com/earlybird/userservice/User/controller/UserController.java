c53a1355e3b9c76c784b65132f8c6c3b68da8ff9
j00r6 <jindory8@gmail.com>
2024-11-06 18:14:37 +0900

    feat : Gateway 에서 토큰 검증 요청을 받기 위한 Controller 구현

--- early-bird/user-service/src/main/java/com/earlybird/userservice/User/controller/UserController.java
+++ early-bird/user-service/src/main/java/com/earlybird/userservice/User/controller/UserController.java
@@ -1,15 +1,14 @@
 package com.earlybird.userservice.User.controller;
 
+import com.earlybird.userservice.Security.resolver.LoginUserId;
 import com.earlybird.userservice.User.dto.SignInRequest;
+import com.earlybird.userservice.User.entity.User;
 import com.earlybird.userservice.User.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/user")
@@ -26,5 +25,15 @@ public class UserController {
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .body("회원가입 완료");
+    }
+
+    @GetMapping
+    public ResponseEntity<Long> getUser(@LoginUserId Long userId) {
+        User findUser = userService.findVerifyUser(userId);
+        Long returnUserId = findUser.getUserId();
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(returnUserId);
     }
 }
