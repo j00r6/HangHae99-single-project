2c5ee1871247ab3f98f7ff302c72f0c6f9a4883e
j00r6 <jindory8@gmail.com>
2024-11-28 16:43:14 +0900

    fix : Product-service 서비스 단일화, roles 임시 비활성화

--- early-bird/product-service/src/main/java/com/earlybird/productservice/Product/controller/ProductController.java
+++ early-bird/product-service/src/main/java/com/earlybird/productservice/Product/controller/ProductController.java
@@ -25,13 +25,11 @@ public class ProductController {
      * 제품 등록의 경우 일반 회원은 진행하지 못하고
      * 판매자의 경우만 가능하므로, 회원가입시 인증정보에서 roles 를 가져와 검증
      *
-     * @param roles
      * @param request
      * @return
      */
     @PostMapping("/register")
-    public ResponseEntity<String> registerProduct(@RequestHeader("roles") String roles,
-                                                  @RequestBody ProductRequestDto request) {
+    public ResponseEntity<String> registerProduct(@RequestBody ProductRequestDto request) {
         productService.registerProduct(request);
         return ResponseEntity
                 .status(HttpStatus.CREATED)
--- early-bird/product-service/src/main/java/com/earlybird/productservice/ProductServiceApplication.java
+++ early-bird/product-service/src/main/java/com/earlybird/productservice/ProductServiceApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.earlybird.userservice"})
 public class ProductServiceApplication {
 
     public static void main(String[] args) {
